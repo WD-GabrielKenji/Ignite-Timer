373ff914c4d13c9a92a76a6cfb99454241fd2418
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-03-01 20:15:37 -0300

    :sparkles: feat: Pagina Home

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,97 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.main`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`
+
+export const FormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+  font-size: 1.125rem;
+  font-weight: bold;
+`
+const BaseInput = styled.input`
+  height: 2.5rem;
+  padding: 0 0.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  background: transparent;
+  font-weight: bold;
+  font-size: 1.125rem;
+  color: ${(props) => props.theme['gray-100']};
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
+
+export const CountdownContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  line-height: 8rem;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 10rem;
+  color: ${(props) => props.theme['gray-100']};
+  span {
+    padding: 2rem 1rem;
+    border-radius: 8px;
+    background: ${(props) => props.theme['gray-700']};
+  }
+`
+
+export const Separator = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 4rem;
+  padding: 2rem 0;
+  color: ${(props) => props.theme['green-500']};
+  overflow: hidden;
+`
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  font-weight: bold;
+  cursor: pointer;
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
+`
