b07412b22cd181bfd18794281dc576aa5083e248
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-03-02 19:12:32 -0300

    :recycle: :construction: refactor: Comp Countdown e Formulário

---
+++ src/pages/Home/Components/Countdown/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const CountdownContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  line-height: 8rem;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 10rem;
+  color: ${(props) => props.theme['gray-100']};
+  span {
+    padding: 2rem 1rem;
+    border-radius: 8px;
+    background: ${(props) => props.theme['gray-700']};
+  }
+`
+
+export const Separator = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 4rem;
+  padding: 2rem 0;
+  color: ${(props) => props.theme['green-500']};
+  overflow: hidden;
+`
